1a863b7bfb453a560292413949864ec9eccf3048
jarvisYu <t5138169@gmail.com>
2021-07-21 23:46:03 +0800

    修改相關產品

--- src/views/fronted/Product.vue
+++ src/views/fronted/Product.vue
@@ -80,6 +80,28 @@
         </div>
         <hr>
         <h2 class="text-center font-md-xl fw-bold pb-4 pt-2">相關商品</h2>
+        <ul class="row">
+        <li class="col-md-4 col-lg-3" v-for="item in randomProducts" :key="item.id">
+        <div class="card">
+            <a href="#" @click.prevent="goProduct(item.id)">
+            <div
+                class="card-img-top"
+                :style="`background-image: url(${item.imageUrl})`"
+            >
+                <div class="mask">
+                <div class="caption">查看商品</div>
+                </div>
+            </div>
+            <div class="card-body">
+                <span class="text-grizzle">{{ item.category }}</span>
+                <h4 class="card-text my-2 fw-bold font-m">{{ item.title }}</h4>
+                <del>NT${{ item.origin_price }}</del>
+                <p class="font-m mt-2 fw-bold">NT${{ item.price }}</p>
+            </div>
+            </a>
+        </div>
+        </li>
+    </ul>
       </div>
     </section>
     <Footer/>
