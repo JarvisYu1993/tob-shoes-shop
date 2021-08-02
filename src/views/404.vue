91b5125b842dfc0282e9f910d9b2790661a065d2
jarvisYu <t5138169@gmail.com>
2021-08-02 16:38:18 +0800

    修改縮排

--- src/views/404.vue
+++ src/views/404.vue
@@ -7,7 +7,7 @@
           <h2 class="font-lg-4xl text-center text-white fw-bold mb-6 mb-md-8">404</h2>
           <p class="font-md text-center text-white pt-6 pb-4">
             oh~oh 網頁發生錯誤喔
-             </p>
+          </p>
           <div class="d-flex justify-content-center">
             <router-link to="/" class="btn btn-secondary text-white">重新回到首頁</router-link>
           </div>
@@ -19,8 +19,8 @@
   <router-view/>
 </template>
 <script>
-import Navbar from '@/components/Navbar.vue';
-import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/fronted/Navbar.vue';
+import Footer from '@/components/fronted/Footer.vue';
 
 export default {
   components: {
