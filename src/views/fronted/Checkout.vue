c442f331eb5aceaf71d822393fd3156b6c0f5436
jarvisYu <t5138169@gmail.com>
2021-08-08 12:19:36 +0800

    優化

--- src/views/fronted/Checkout.vue
+++ src/views/fronted/Checkout.vue
@@ -14,131 +14,130 @@
             </router-link>
           </div>
           <Form ref="form" v-slot="{ errors }" @submit="createOrder">
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="email" class="form-label fw-bold">
-                <span class="text-danger">*</span>
-                Email
-            </label>
-            <Field
-            id="email"
-            name="email"
-            type="email"
-            class="form-control"
-            :class="{ 'is-invalid': errors['email'] }"
-            placeholder="請輸入 Email"
-            rules="email|required"
-            v-model.trim="form.user.email"
-            ></Field>
-            <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
-        </div>
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="name" class="form-label fw-bold">
-                <span class="text-danger">*</span>
-                收件人姓名
-            </label>
-            <Field
-            id="name"
-            name="姓名"
-            type="text"
-            class="form-control"
-            :class="{ 'is-invalid': errors['姓名'] }"
-            placeholder="請輸入姓名"
-            rules="required"
-            v-model.trim="form.user.name"
-            ></Field>
-            <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
-        </div>
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="tel" class="form-label fw-bold">
-                <span class="text-danger">*</span>
-                收件人電話
-            </label>
-            <Field
-            id="tel"
-            name="電話"
-            type="text"
-            class="form-control"
-            :class="{ 'is-invalid': errors['電話'] }"
-            placeholder="請輸入電話"
-            rules="required"
-            v-model.trim="form.user.tel"
-            ></Field>
-            <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
-        </div>
-
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="address" class="form-label fw-bold">
-                <span class="text-danger">*</span>
-                收件人地址
-            </label>
-            <Field
-            id="address"
-            name="地址"
-            type="text"
-            class="form-control"
-            :class="{ 'is-invalid': errors['地址'] }"
-            placeholder="請輸入地址"
-            rules="required"
-            v-model.trim="form.user.address"
-            ></Field>
-            <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
-        </div>
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="payment" class="form-label fw-bold">
-                <span class="text-danger">*</span>
-                付款方式
-            </label>
-            <Field
-            id="payment"
-            name="付款方式"
-            as="select"
-            class="form-control"
-            :class="{ 'is-invalid': errors['付款方式'] }"
-            rules="required"
-            v-model.trim="form.user.payment"
-            >
-            <option disabled>請選擇付款方式</option>
-            <option value="ATM轉帳">ATM轉帳</option>
-            <option value="信用卡">信用卡</option>
-            <option value="現金付款">現金付款</option>
-            <option value="ApplePay">ApplePay</option>
-            <option value="SamsuangPay">SamsuangPay</option>
-            <option value="GooglePay">GooglePay</option>
-            <option value="LinePay">LinePay</option>
-            </Field>
-            <ErrorMessage name="付款方式" class="invalid-feedback"></ErrorMessage>
-        </div>
-        <div class="mb-3 d-flex flex-column align-items-start">
-            <label for="message" class="form-label fw-bold">留言</label>
-            <textarea
-            name=""
-            id="message"
-            class="form-control"
-            cols="30"
-            rows="10"
-            v-model="form.message"
-            ></textarea>
-        </div>
-        <div class="d-flex align-items-center justify-content-between">
-            <router-link to="/cart" class="btn btn-outline-primary">返回購物車</router-link>
-            <button class="btn btn-danger" :disabled="!isErrors">送出訂單</button>
-        </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="email" class="form-label fw-bold">
+                    <span class="text-danger">*</span>
+                    Email
+                </label>
+                <Field
+                id="email"
+                name="email"
+                type="email"
+                class="form-control"
+                :class="{ 'is-invalid': errors['email'] }"
+                placeholder="請輸入 Email"
+                rules="email|required"
+                v-model.trim="form.user.email"
+                ></Field>
+                <ErrorMessage name="email" class="invalid-feedback"></ErrorMessage>
+            </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="name" class="form-label fw-bold">
+                    <span class="text-danger">*</span>
+                    收件人姓名
+                </label>
+                <Field
+                id="name"
+                name="姓名"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors['姓名'] }"
+                placeholder="請輸入姓名"
+                rules="required"
+                v-model.trim="form.user.name"
+                ></Field>
+                <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+            </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="tel" class="form-label fw-bold">
+                    <span class="text-danger">*</span>
+                    收件人電話
+                </label>
+                <Field
+                id="tel"
+                name="電話"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors['電話'] }"
+                placeholder="請輸入電話"
+                rules="required"
+                v-model.trim="form.user.tel"
+                ></Field>
+                <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+            </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="address" class="form-label fw-bold">
+                    <span class="text-danger">*</span>
+                    收件人地址
+                </label>
+                <Field
+                id="address"
+                name="地址"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors['地址'] }"
+                placeholder="請輸入地址"
+                rules="required"
+                v-model.trim="form.user.address"
+                ></Field>
+                <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+            </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="payment" class="form-label fw-bold">
+                    <span class="text-danger">*</span>
+                    付款方式
+                </label>
+                <Field
+                id="payment"
+                name="付款方式"
+                as="select"
+                class="form-control"
+                :class="{ 'is-invalid': errors['付款方式'] }"
+                rules="required"
+                v-model.trim="form.user.payment"
+                >
+                <option disabled>請選擇付款方式</option>
+                <option value="ATM轉帳">ATM轉帳</option>
+                <option value="信用卡">信用卡</option>
+                <option value="現金付款">現金付款</option>
+                <option value="ApplePay">ApplePay</option>
+                <option value="SamsuangPay">SamsuangPay</option>
+                <option value="GooglePay">GooglePay</option>
+                <option value="LinePay">LinePay</option>
+                </Field>
+                <ErrorMessage name="付款方式" class="invalid-feedback"></ErrorMessage>
+            </div>
+            <div class="mb-3 d-flex flex-column align-items-start">
+                <label for="message" class="form-label fw-bold">留言</label>
+                <textarea
+                name=""
+                id="message"
+                class="form-control"
+                cols="30"
+                rows="10"
+                v-model="form.message"
+                ></textarea>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+                <router-link to="/cart" class="btn btn-outline-primary">返回購物車</router-link>
+                <button class="btn btn-danger" :disabled="!isErrors">送出訂單</button>
+            </div>
           </Form>
         </div>
         <div class="col-md-5 order-1 order-md-2 mb-4 mb-md-0">
-            <div class="border p-4">
-                <h3 class="fw-bold font-md border-bottom pb-3 mb-3">訂單明細</h3>
+            <div class="border p-2 py-4 p-lg-4">
+              <h3 class="fw-bold font-md border-bottom pb-3 mb-3">訂單明細</h3>
               <div class="d-flex align-items-center justify-content-between mb-2"
               v-for="item in cart.carts" :key="item.id">
-              <div class="d-flex align-items-center mb-2">
+              <div class="d-flex align-items-center">
                 <img class="order-img" :src="item.product.imageUrl"
                 :alt="item.product.title">
                 <div class="p-2">
-                  <h4 class="fw-bold">{{ item.product.title }}</h4>
-                  <h5 class="font-s d-flex align-items-center">{{ item.size.size }}
+                  <h4 class="font-md-normal fw-bold">{{ item.product.title }}</h4>
+                  <h5 class="font-md-normal d-flex align-items-center">{{ item.size.size }}
                     <span class="ms-1">x{{ item.qty }}</span>
                   </h5>
-                  <h5 class="font-s">{{ item.product.content }}</h5>
+                  <h5 class="font-md-normal">{{ item.product.content }}</h5>
                 </div>
               </div>
                 <p>NT${{ $toCurrency(item.final_total) }}</p>
@@ -162,11 +161,11 @@
         </div>
       </div>
     </div>
-  <router-view/>
 </template>
 
 <script>
 import PulseLoader from '@/components/PulseLoader.vue';
+import emitter from '@/assets/javascript/emitter';
 
 export default {
   name: 'Cart',
@@ -223,6 +222,7 @@ export default {
             if (res.data.success) {
               this.$refs.form.resetForm();
               this.id = res.data.orderId;
+              emitter.emit('product-cart');
               this.$swal.fire(
                 {
                   position: 'top',
@@ -256,7 +256,13 @@ export default {
     },
   },
   mounted() {
+    emitter.on('product-cart', () => {
+      this.getCart();
+    });
     this.getCart();
+  },
+  unmounted() {
+    emitter.off('product-cart', () => this.getCart());
   },
 };
 </script>
