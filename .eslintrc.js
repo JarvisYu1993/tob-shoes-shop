e84aaf5a1043e6b7fbac0fdfe54e28a98f7b3f2b
jarvisYu <t5138169@gmail.com>
2021-08-08 12:14:51 +0800

    修改 eslint 規則

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,10 +10,25 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+      }
+    },
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     "no-param-reassign": ['error', { 'props': false }],
-    "no-else-return": ["error", { "allowElseIf": false }]
+    "no-else-return": ["error", { "allowElseIf": false }],
+    "vue/no-unused-components": "off",
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+    }],
   },
 };
